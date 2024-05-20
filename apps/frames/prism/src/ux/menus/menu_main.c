a5c1f13608897ae5c84b40a0a7f78536579e45a9
austanss <austanssical@gmail.com>
2024-05-20 19:11:15 -0400

    perf: rework menu design algorithm and format

--- apps/frames/prism/src/ux/menus/menu_main.c
+++ apps/frames/prism/src/ux/menus/menu_main.c
@@ -8,157 +8,86 @@
 #define MENU_MAIN_WHITE_OVERRIDE TEXT_COLOR_WHITE
 #define MENU_MAIN_BLACK_OVERRIDE TEXT_COLOR_BLACK
 
-struct _menu_main_option {
-    const char* text;
-    const struct _menu_main_option* above;    // Up key
-    const struct _menu_main_option* below;    // Down key
-    const struct _menu_main_option* left;     // Left key
-    const struct _menu_main_option* right;    // Right key
-    const struct _menu_main_option* parent;   // Backspace key
-    const struct _menu_main_option* child;    // Enter key
-    const void (*invoke_entry)();             // Space key
-    uint64_t last_draw;             // Last time the option was drawn (iterator)
-    uint64_t p_col;
-    uint64_t p_row;
-    uint8_t color;
-};
-
-static struct _menu_main_option _reboot_option;
-static struct _menu_main_option _nop_option;
-
-static struct _menu_main_option* __topleft_option = &_nop_option;
-static struct _menu_main_option* __current_option = NULL;
-
-static struct _menu_main_option _nop_option = {
-    .text = "Do nothing",
-    .above = NULL,  // No wraparound
-    .below = &_reboot_option,   // Go down
-    .left = NULL,
-    .right = NULL,
-    .parent = NULL,
-    .child = NULL,
-    .invoke_entry = NULL,
-    .last_draw = 0,
-    .p_col = 0,     // Do not set them by default, the drawing algorithm will do that
-    .p_row = 0,     // Do not set them by default, the drawing algorithm will do that
-    .color = TEXT_COLOR_COMBO(MENU_MAIN_WHITE_OVERRIDE, MENU_MAIN_BLACK_OVERRIDE)
-};
-
-static struct _menu_main_option _reboot_option = {
-    .text = "Reboot violently",
-    .above = &_nop_option,   // Go up
-    .below = NULL,
-    .left = NULL,
-    .right = NULL,
-    .parent = NULL,
-    .child = NULL,
-    .invoke_entry = NULL,
-    .last_draw = 0,
-    .p_col = 0,
-    .p_row = 0,
-    .color = TEXT_COLOR_COMBO(TEXT_COLOR_RED, MENU_MAIN_BLACK_OVERRIDE)
-};
-
-
-#define INVERT_COLOR_BG(color) (color ^ 0x80)
-static void _menu_main_highlight_option(const struct _menu_main_option* option) {
-    // Highlight the option
-    menu_render_string_at(option->p_col, option->p_row, option->text, INVERT_COLOR_BG(option->color));
-}
-
-static void _menu_main_unhighlight_option(const struct _menu_main_option* option) {
-    // Unhighlight the option
-    menu_render_string_at(option->p_col, option->p_row, option->text, option->color);
-}
-
-static void _menu_main_select_option(const struct _menu_main_option* option) {
-    if (option == NULL) {
-        return;
-    }
-    // Select the option
-    _menu_main_unhighlight_option(__current_option);
-    __current_option = (struct _menu_main_option *)option;
-    _menu_main_highlight_option(__current_option);
-}
-
 #define MENU_MAIN_INDENT_SIZE 4     // Indent size in spaces
-#define MENU_MAIN_DEFAULT_INDENT 0  // No indent by default
 #define MENU_MAIN_MARGIN_TOP 2      // Margin in rows from the top of the screen
 #define MENU_MAIN_MARGIN_BOTTOM 2   // Margin in rows from the bottom of the screen
 #define MENU_MAIN_MARGIN_LEFT 4     // Margin in columns from the left of the screen
 #define MENU_MAIN_MARGIN_RIGHT 4    // Margin in columns from the right of the screen
-#define MENU_MAIN_MARGIN_OPTION 1   // Margin in rows between options
-#define MENU_MAIN_ROW_INCREMENT MENU_MAIN_MARGIN_OPTION+1   // Row increment for the drawing algorithm
+
+static menu_item_t* __selected_option = NULL;
+
+static menu_item_t __menu_main_option_1;
+static menu_item_t __menu_main_option_2;
+static menu_item_t __menu_main_option_3;
+static menu_item_t __menu_main_option_4;
+
+static menu_item_t __menu_main_option_1 = {
+    .next = &__menu_main_option_2,   .perform = NULL_MENU_TASK_HANDLER,
+    .text = "Option 1",
+    .col = 0,   .row = 0,   .color = TEXT_COLOR_RED,
+    .up = &__menu_main_option_2,    .down = &__menu_main_option_2,   .left = NULL,    .right = &__menu_main_option_3
+};
+
+static menu_item_t __menu_main_option_2 = {
+    .next = &__menu_main_option_3,   .perform = NULL_MENU_TASK_HANDLER,
+    .text = "Option 2",
+    .col = 0,   .row = 6,   .color = TEXT_COLOR_CYAN,
+    .up = &__menu_main_option_1,    .down = &__menu_main_option_1,   .left = NULL,    .right = NULL
+};
+
+static menu_item_t __menu_main_option_3 = {
+    .next = &__menu_main_option_4,   .perform = NULL_MENU_TASK_HANDLER,
+    .text = "Option 3",
+    .col = MENU_MAIN_INDENT_SIZE,   .row = 2,   .color = TEXT_COLOR_GREEN,
+    .up = &__menu_main_option_4,    .down = &__menu_main_option_4,   .left = &__menu_main_option_1,    .right = NULL
+};
+
+static menu_item_t __menu_main_option_4 = {
+    .next = NULL,   .perform = NULL_MENU_TASK_HANDLER,
+    .text = "Option 4",
+    .col = MENU_MAIN_INDENT_SIZE,   .row = 4,   .color = TEXT_COLOR_MAGENTA,
+    .up = &__menu_main_option_3,    .down = &__menu_main_option_3,   .left = &__menu_main_option_1,    .right = NULL
+};
+
+#define INVERT_COLOR_BG(color) (color ^ 0x80)
+static void _menu_main_highlight_option(const menu_item_t* option) {
+    // Highlight the option
+    menu_render_string_at(MENU_MAIN_MARGIN_LEFT + option->col, MENU_MAIN_MARGIN_TOP + option->row, option->text, INVERT_COLOR_BG(option->color));
+}
+
+static void _menu_main_unhighlight_option(const menu_item_t* option) {
+    // Unhighlight the option
+    menu_render_string_at(MENU_MAIN_MARGIN_LEFT + option->col, MENU_MAIN_MARGIN_TOP + option->row, option->text, option->color);
+}
+
+static void _menu_main_select_option(const menu_item_t* option) {
+    switch ((uint64_t)option) {
+        case 0:
+            return;
+        default:
+            _menu_main_unhighlight_option(__selected_option);
+            __selected_option = (menu_item_t*)option;
+            _menu_main_highlight_option(__selected_option);
+            return;
+    }
+}
+
+// NULL_MENU_TASK_HANDLER null op handler for menu items 
+void __menu_option_do_nothing(void) {
+    return;
+}
 
 static bool _menu_drawn = false;
-static uint64_t _menu_last_draw = 0;
 static void _menu_main_draw() {
     // Draw the menu
+    _menu_drawn = false;
     text_render_clear();
-    struct _menu_main_option* option_iterator = __topleft_option;
-    uint64_t row = MENU_MAIN_MARGIN_TOP, col = MENU_MAIN_MARGIN_LEFT + (MENU_MAIN_INDENT_SIZE * MENU_MAIN_DEFAULT_INDENT);
-    bool drawing = true;
-    _menu_last_draw++;
-    // Extremely directional algorithm to draw the menu
-    // Goes into, right, and down, in that priority
-    // Goes left, and out, in that priority
-    while (drawing) {
-        if (option_iterator->last_draw >= _menu_last_draw) {
-            drawing = false;
-            break;
-        }
-
-        size_t text_length = strlen(option_iterator->text);
-        menu_render_string_at(col, row, option_iterator->text, option_iterator->color);
-        
-        option_iterator->p_col = col;
-        option_iterator->p_row = row;
-        option_iterator->last_draw = _menu_last_draw;
-
-        if (option_iterator->child != NULL) {
-            col += MENU_MAIN_INDENT_SIZE;
-            option_iterator = (struct _menu_main_option *)option_iterator->child;
-        } else if (option_iterator->right != NULL) {
-            col += text_length;
-            col += (MENU_MAIN_INDENT_SIZE - (col % MENU_MAIN_INDENT_SIZE));
-            option_iterator = (struct _menu_main_option *)option_iterator->right;
-        } else if (option_iterator->left != NULL) {
-            col -= MENU_MAIN_INDENT_SIZE;
-            option_iterator = (struct _menu_main_option *)option_iterator->left;
-            while (option_iterator->last_draw >= _menu_last_draw) {
-                if (option_iterator->left != NULL) {
-                    col -= MENU_MAIN_INDENT_SIZE;
-                    option_iterator = (struct _menu_main_option *)option_iterator->left;
-                } else if (option_iterator->below != NULL) {
-                    row += MENU_MAIN_ROW_INCREMENT;
-                    option_iterator = (struct _menu_main_option *)option_iterator->below;
-                } else {
-                    drawing = false;
-                    break;
-                }
-            }
-        } else if (option_iterator->below != NULL) {
-            row += MENU_MAIN_ROW_INCREMENT;
-            option_iterator = (struct _menu_main_option *)option_iterator->below;
-        } else if (option_iterator->parent != NULL) {
-            col -= MENU_MAIN_INDENT_SIZE;
-            row += MENU_MAIN_ROW_INCREMENT;
-            option_iterator = (struct _menu_main_option *)option_iterator->parent;
-            while (option_iterator->last_draw >= _menu_last_draw) {
-                if (option_iterator->parent != NULL) {
-                    col -= MENU_MAIN_INDENT_SIZE;
-                    option_iterator = (struct _menu_main_option *)option_iterator->parent;
-                } else {
-                    drawing = false;
-                    break;
-                }
-            }
-        } else {
-            drawing = false;
-        }
+    
+    for (menu_item_t* item = &__menu_main_option_1; item != NULL; item = (menu_item_t *)item->next) {
+        menu_render_string_at(MENU_MAIN_MARGIN_LEFT + item->col, MENU_MAIN_MARGIN_TOP + item->row, item->text, item->color);
     }
     _menu_drawn = true;
-    _menu_main_select_option(__current_option);
+    _menu_main_select_option(__selected_option);
 }
 
 static bool _menu_needs_update = true;
@@ -167,8 +96,8 @@ void _menu_initialize() {
     if (_menu_initialized) {
         return;
     }
-    __topleft_option = &_nop_option;
-    __current_option = __topleft_option;
+    __selected_option = &__menu_main_option_1;
+    _menu_main_select_option(__selected_option);
     _menu_needs_update = true;
     _menu_initialized = true;
 }
@@ -185,32 +114,24 @@ void menu_main_update() {
 }
 
 void menu_main_handle_key(uint8_t keycode) {
-    if (__current_option == NULL) {
+    if (__selected_option == NULL) {
         return;
     }
     switch (keycode) {
         case HID_KEYCODE_UP:
-            _menu_main_select_option(__current_option->above);
+            _menu_main_select_option(__selected_option->up);
             break;
         case HID_KEYCODE_DOWN:
-            _menu_main_select_option(__current_option->below);
+            _menu_main_select_option(__selected_option->down);
             break;
         case HID_KEYCODE_LEFT:
-            _menu_main_select_option(__current_option->left);
+            _menu_main_select_option(__selected_option->left);
             break;
         case HID_KEYCODE_RIGHT:
-            _menu_main_select_option(__current_option->right);
-            break;
-        case HID_KEYCODE_BACKSPACE:
-            _menu_main_select_option(__current_option->parent);
+            _menu_main_select_option(__selected_option->right);
             break;
         case HID_KEYCODE_ENTER:
-            _menu_main_select_option(__current_option->child);
-            break;
-        case HID_KEYCODE_SPACE:
-            if (__current_option->invoke_entry != NULL) {
-                __current_option->invoke_entry();
-            }
+            __selected_option->perform();
             break;
         default:
             break;
--- apps/frames/prism/src/ux/menus/menus.h
+++ apps/frames/prism/src/ux/menus/menus.h
@@ -12,3 +12,18 @@ extern tui_menu_handle_t* menus_menu_main;
 
 void menu_render_string_at(uint64_t col, uint64_t row, const char* str, uint8_t color);
 #define MENU_STRING_INDENT_SIZE 4
+void __menu_option_do_nothing(void);
+#define NULL_MENU_TASK_HANDLER (const void(*)(void))&__menu_option_do_nothing
+
+typedef struct s_menu_item {
+    const struct s_menu_item* next;
+    const void (*perform)(void);
+    const char* text;
+    const uint64_t col;
+    const uint64_t row;
+    const uint8_t color;
+    const struct s_menu_item* up;       // This is used to navigate the menu, null if not present
+    const struct s_menu_item* down;     // This is used to navigate the menu, null if not present
+    const struct s_menu_item* left;     // This is used to navigate the menu, null if not present
+    const struct s_menu_item* right;    // This is used to navigate the menu, null if not present
+} menu_item_t;
