f8733ea6e9a917e4a7f1e5b15dc9eeb660ea27f4
austanss <austanssical@gmail.com>
2024-07-01 18:47:24 -0400

    partial: add ahci start/stop commands

--- kernel/src/iodev/disk/ahci.c
+++ kernel/src/iodev/disk/ahci.c
@@ -1,28 +1,44 @@
 #include "iodev/disk/ahci.h"
 #include "iodev/pci/confrw.h"
 #include "mem/paging/paging.h"
+#include "mem/pmm/pmm.h"
+#include <string.h>
+
+//
+// Design largely ripped from Absurdponcho/PonchoOS (Public domain)
+// However, severely modified due to serious bugs in original source
+// Also, translated from C++ to C, and fit into this project's style
+//
 
 #define AHCI_PCI_DEVICE_CLASS       0x01
 #define AHCI_PCI_DEVICE_SUBCLASS    0x06
 #define AHCI_PCI_DEVICE_INTERFACE   0x01
 
-typedef enum {
-    AHCI_CONTROLLER_PORT_TYPE_NONE,
-    AHCI_CONTROLLER_PORT_TYPE_SATA,
-    AHCI_CONTROLLER_PORT_TYPE_SEMB,
-    AHCI_CONTROLLER_PORT_TYPE_PM,
-    AHCI_CONTROLLER_PORT_TYPE_SATAPI,
-} ahci_controller_port_type_t;
+#define AHCI_HBA_PxCMD_CR   0x8000
+#define AHCI_HBA_PxCMD_FRE  0x0010
+#define AHCI_HBA_PxCMD_ST   0x0001
+#define AHCI_HBA_PxCMD_FR   0x4000
+
+#define AHCI_CONTROLLER_PORT_COUNT  0x20
+#define AHCI_CMD_HEADERS_PER_LIST   0x20
 
 static bool _ahci_driver_loaded = false;
 static bool _ahci_controller_checked = false;
 
 static ahci_bus_handle_t _ahci_bus_handle = {
     .hba_mem_abar = NULL,
-    .pci_device = NULL
+    .pci_device = NULL,
+    .drive_ports = NULL
 };
 
+void ahci_controller_configure_all_ports();
+
 const bool _ahci_driver_load_pci() {
+    if (_ahci_controller_checked) {
+        return _ahci_driver_loaded;
+    }
+
+    _ahci_driver_loaded = false;
     _ahci_bus_handle.pci_device = find_device(AHCI_PCI_DEVICE_CLASS, AHCI_PCI_DEVICE_SUBCLASS, AHCI_PCI_DEVICE_INTERFACE)->base;
     if (_ahci_bus_handle.pci_device == NULL) {
         _ahci_controller_checked = true;
@@ -35,15 +51,18 @@ const bool _ahci_driver_load_pci() {
         return false;
     }
 
+    _ahci_bus_handle.drive_ports = (ahci_drive_handle_t *)pmm_alloc_page();
+    memset(_ahci_bus_handle.drive_ports, 0, sizeof(ahci_drive_handle_t) * AHCI_CONTROLLER_PORT_COUNT);
+
     _ahci_controller_checked = true;
-    return (paging_map_page((void *)_ahci_bus_handle.hba_mem_abar, (void *)_ahci_bus_handle.hba_mem_abar, PAGING_FLAGS_KERNEL_PAGE) == NULL);
+    _ahci_driver_loaded = (paging_map_page((void *)_ahci_bus_handle.hba_mem_abar, (void *)_ahci_bus_handle.hba_mem_abar, PAGING_FLAGS_MMIO_PAGE) != NULL);
+
+    ahci_controller_configure_all_ports();
+    return _ahci_driver_loaded;
 }
 
 const bool ahci_driver_available() {
-    if (!_ahci_controller_checked) {
-        _ahci_driver_loaded = _ahci_driver_load_pci();
-    }
-    return _ahci_driver_loaded;
+    return _ahci_driver_load_pci() && _ahci_driver_loaded;
 }
 
 #define HBA_PORT_DEV_DETECT_PRESENT     0x03
@@ -67,10 +86,10 @@ ahci_controller_port_type_t ahci_controller_classify_port(const ahci_hba_port_t*
     }
 
     switch (sata_signature) {
-        case AHCI_SATA_SIGNATURE_ATAPI:
-            return AHCI_CONTROLLER_PORT_TYPE_SATAPI;
         case AHCI_SATA_SIGNATURE_ATA:
             return AHCI_CONTROLLER_PORT_TYPE_SATA;
+        case AHCI_SATA_SIGNATURE_ATAPI:
+            return AHCI_CONTROLLER_PORT_TYPE_SATAPI;
         case AHCI_SATA_SIGNATURE_SEMB:
             return AHCI_CONTROLLER_PORT_TYPE_SEMB;
         case AHCI_SATA_SIGNATURE_PM:
@@ -83,6 +102,18 @@ ahci_controller_port_type_t ahci_controller_classify_port(const ahci_hba_port_t*
 void ahci_controller_register_port(const uint32_t port) {
     ahci_hba_port_t* port_handle = &_ahci_bus_handle.hba_mem_abar->ports[port];
     ahci_controller_port_type_t port_type = ahci_controller_classify_port(port_handle);
+
+    switch (port_type) {
+        case AHCI_CONTROLLER_PORT_TYPE_SATA:
+        case AHCI_CONTROLLER_PORT_TYPE_SATAPI:
+            break;
+        default:
+            return;
+    }
+
+    _ahci_bus_handle.drive_ports[port].port_number = port;
+    _ahci_bus_handle.drive_ports[port].port_type = port_type;
+    _ahci_bus_handle.drive_ports[port].port_hba = port_handle;
 }
 
 void ahci_controller_probe_ports() {
@@ -92,9 +123,96 @@ void ahci_controller_probe_ports() {
 
     uint32_t ports_implemented = _ahci_bus_handle.hba_mem_abar->ports_implemented;
 
-    for (uint32_t i = 0; i < 32; i++) {
+    for (uint32_t i = 0; i < AHCI_CONTROLLER_PORT_COUNT; i++) {
         if (ports_implemented & (1 << i)) {
             ahci_controller_register_port(i);
         }
+    }
+}
+
+void ahci_controller_configure_port(uint32_t port_no) {
+    if (!ahci_driver_available()) {
+        return;
+    }
+
+    ahci_drive_handle_t* drive = &_ahci_bus_handle.drive_ports[port_no];
+    if (drive->port_hba == NULL || drive->port_type == AHCI_CONTROLLER_PORT_TYPE_NONE || drive->port_number != port_no) {
+        return;
+    }
+
+    ahci_drive_command_stop(drive);
+
+    void* new_cmd_base = pmm_alloc_page();
+    void* new_fis_base = pmm_alloc_page();
+    memset(new_cmd_base, 0, PAGING_PAGE_SIZE);
+    memset(new_fis_base, 0, PAGING_PAGE_SIZE);
+    drive->port_hba->command_list_base_lower = (uint32_t)((uintptr_t)new_cmd_base >> 00);
+    drive->port_hba->command_list_base_upper = (uint32_t)((uintptr_t)new_cmd_base >> 32);
+    drive->port_hba->fis_base_lower = (uint32_t)((uintptr_t)new_fis_base >> 00);
+    drive->port_hba->fis_base_upper = (uint32_t)((uintptr_t)new_fis_base >> 32);
+
+#define DESIRED_PRDT_LENGTH 8
+
+    ahci_hba_command_header_t* cmd_header = (ahci_hba_command_header_t *)new_cmd_base;
+
+    size_t cmd_table_size = sizeof(ahci_hba_command_table_t) + ((sizeof(ahci_hba_prdt_entry_t) * DESIRED_PRDT_LENGTH));
+    void* cmd_tables_addr = pmm_alloc_pool(cmd_table_size / PAGING_PAGE_SIZE);
+    memset(cmd_tables_addr, 0x00, cmd_table_size * AHCI_CMD_HEADERS_PER_LIST);
+
+    for (uint32_t i = 0; i < AHCI_CMD_HEADERS_PER_LIST; i++) {
+        cmd_header[i].prdt_length = DESIRED_PRDT_LENGTH;
+
+        uint64_t cmd_table_base = (uint64_t)cmd_tables_addr + (i * cmd_table_size);
+        cmd_header[i].command_table_base_lower = (uint32_t)(cmd_table_base >> 00);
+        cmd_header[i].command_table_base_upper = (uint32_t)(cmd_table_base >> 32);
+    }
+
+    ahci_drive_command_start(drive);
+
+    drive->data_buffer = pmm_alloc_page();
+    memset(drive->data_buffer, 0, PAGING_PAGE_SIZE);
+}
+
+static bool __ports_configured = false;
+
+void ahci_controller_configure_all_ports() {
+    if (!__ports_configured) {
+        return;
+    }
+    for (uint32_t i = 0; i < AHCI_CONTROLLER_PORT_COUNT; i++) {
+        ahci_controller_configure_port(i);
+    }
+    __ports_configured = true;
+}
+
+void ahci_drive_command_start(const ahci_drive_handle_t* drive) {
+    if (!ahci_driver_available()) {
+        return;
+    }
+
+    while (drive->port_hba->command_status & AHCI_HBA_PxCMD_CR) {
+        __asm__ volatile ("hlt");
+    }
+
+    drive->port_hba->command_status |= AHCI_HBA_PxCMD_FRE;
+    drive->port_hba->command_status |= AHCI_HBA_PxCMD_ST;
+}
+
+void ahci_drive_command_stop(const ahci_drive_handle_t* drive) {
+    if (!ahci_driver_available()) {
+        return;
+    }
+
+    drive->port_hba->command_status &= ~AHCI_HBA_PxCMD_ST;
+    drive->port_hba->command_status &= ~AHCI_HBA_PxCMD_FRE;
+
+    while (true) {
+        __asm__ volatile ("hlt");
+        if (drive->port_hba->command_status & AHCI_HBA_PxCMD_FR) 
+            continue;
+        if (drive->port_hba->command_status & AHCI_HBA_PxCMD_CR) 
+            continue;
+
+        break;
     }
 }
--- kernel/src/iodev/disk/ahci.h
+++ kernel/src/iodev/disk/ahci.h
@@ -3,6 +3,25 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum {
+    AHCI_CONTROLLER_PORT_TYPE_NONE,
+    AHCI_CONTROLLER_PORT_TYPE_SATA,
+    AHCI_CONTROLLER_PORT_TYPE_SATAPI,
+    AHCI_CONTROLLER_PORT_TYPE_SEMB,
+    AHCI_CONTROLLER_PORT_TYPE_PM,
+} ahci_controller_port_type_t;
+
+typedef enum {
+    AHCI_FIS_TYPE_REG_H2D   = 0x27,
+    AHCI_FIS_TYPE_REG_D2H   = 0x34,
+    AHCI_FIS_TYPE_DMA_ACT   = 0x39,
+    AHCI_FIS_TYPE_DMA_SETUP = 0x41,
+    AHCI_FIS_TYPE_DATA      = 0x46,
+    AHCI_FIS_TYPE_BIST      = 0x58,
+    AHCI_FIS_TYPE_PIO_SETUP = 0x5F,
+    AHCI_FIS_TYPE_DEV_BITS  = 0xA1,
+} ahci_controller_fis_type_t;
+
 typedef struct {
     uint32_t    command_list_base_lower;
     uint32_t    command_list_base_upper;
@@ -23,7 +42,7 @@ typedef struct {
     uint32_t    fis_based_switch_control;
     uint32_t    reserved_block[0x0B];
     uint32_t    vendor_specific[0x04];
-} ahci_hba_port_t;
+} __attribute__((__packed__)) ahci_hba_port_t;
 
 typedef struct {
     uint32_t        host_capabilities;
@@ -39,21 +58,84 @@ typedef struct {
     uint32_t        bios_handoff_control_status;
     uint8_t         reserved_block[0x74];
     uint8_t         vendor_specific[0x60];
-    ahci_hba_port_t ports[1];
-} ahci_hba_mem_t;
+    ahci_hba_port_t ports[0];
+} __attribute__((__packed__)) ahci_hba_mem_t;
+
+typedef struct ahci_hba_prdt_entry_t {
+    uint32_t    data_base_lower;
+    uint32_t    data_base_upper;
+    uint32_t    reserved;
+
+    uint32_t    byte_count          :   22;
+    uint32_t    reserved_bits       :   9;
+    uint32_t    interrupt_please    :   1;
+} __attribute__((__packed__)) ahci_hba_prdt_entry_t;
 
 typedef struct {
-    ahci_hba_mem_t*     hba_mem_abar;
-    pci_dev_header_t*   pci_device;
-} ahci_bus_handle_t;
+    uint8_t                 command_fis[0x40];
+    uint8_t                 atapi_command[0x10];
+    uint8_t                 reserved[0x30];
+    ahci_hba_prdt_entry_t   prdt_entries[0];
+} __attribute__((__packed__)) ahci_hba_command_table_t;
 
-typedef struct s_ahci_drive_handle {
-    struct s_ahci_drive_handle* next;
+typedef struct {
+    uint8_t     command_fis_length  :   5;
+    uint8_t     atapi_field         :   1;
+    uint8_t     write_field         :   1;
+    uint8_t     prefetchable        :   1;
 
+    uint8_t     reset_field         :   1;
+    uint8_t     bist_field          :   1;
+    uint8_t     clear_busy          :   1;
+    uint8_t     reserved_bits       :   1;
+    uint8_t     port_multiplier     :   1;
+    
+    uint16_t    prdt_length;
+    uint32_t    prdb_count;
+    uint32_t    command_table_base_lower;
+    uint32_t    command_table_base_upper;
+    uint32_t    reserved_block[4];
+} __attribute__((__packed__)) ahci_hba_command_header_t;
+
+typedef struct {
+    uint8_t     fis_type;
+
+    uint8_t     port_multiplier :   4;
+    uint8_t     reserved        :   3;
+    uint8_t     command_control :   1;
+
+    uint8_t     command;
+    uint8_t     feature_low;
+    uint8_t     lba_0;
+    uint8_t     lba_1;
+    uint8_t     lba_2;
+    uint8_t     device_register;
+    uint8_t     lba_3;
+    uint8_t     lba_4;
+    uint8_t     lba_5;
+    uint8_t     feature_high;
+    uint8_t     count_low;
+    uint8_t     count_high;
+    uint8_t     iso_command_completion;
+    uint8_t     control;
+    uint8_t     reserved_block[0x04];
+} __attribute__((__packed__)) ahci_fis_reg_h2d_t;
+
+typedef struct {
+    ahci_hba_port_t*            port_hba;
+    uint32_t                    port_number;
+    ahci_controller_port_type_t port_type;
+    void*                       data_buffer;
 } ahci_drive_handle_t;
 
-// Returns null if no drives are found
-const ahci_drive_handle_t* ahci_get_first_drive();
+typedef struct {
+    ahci_hba_mem_t*         hba_mem_abar;
+    pci_dev_header_t*       pci_device;
+    ahci_drive_handle_t*    drive_ports;
+} ahci_bus_handle_t;
+
+void ahci_drive_command_start(const ahci_drive_handle_t* drive);
+void ahci_drive_command_stop(const ahci_drive_handle_t* drive);
 
 // Returns false if driver is not available, or there is no AHCI controller
 const bool ahci_driver_available();
